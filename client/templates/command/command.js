717fa373150e0c472af51349204211c494dbb4a4
Matthew Robinson <robinson.me@gmail.com>
2015-12-10 07:06:54 -0500

    Purchase multiples

--- client/templates/command/command.js
+++ client/templates/command/command.js
@@ -30,5 +30,18 @@ Template.command.events({
   },
   'click input.buy': function(event) {
     Meteor.call('purchase', event.target.id);
+  },
+  'click input.buyTen': function(event) {
+    Meteor.call('purchase', event.target.id, 10);
+  },
+  'click input.buyHun': function(event) {
+    Meteor.call('purchase', event.target.id, 100);
+  },
+  'click input.buyMax': function(event) {
+    var worker = Workers.findOne({name: event.target.id});
+    var currentUser = Meteor.userId();
+    var player = Players.findOne({createdBy: currentUser}, {fields: {money: 1}});
+    var max = Math.floor(player.money / worker.cost);
+    Meteor.call('purchase', event.target.id, max);
   }
 });
